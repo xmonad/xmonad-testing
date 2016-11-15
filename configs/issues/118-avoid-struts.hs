ee6f6751c1522120c98f09dce5432767938cc445
Peter Jones <pjones@devalot.com>
2016-11-15 16:13:38 -0700

    Darn, X.C.Desktop already calls `docs' which sets up the layout hook

--- configs/issues/118-avoid-struts.hs
+++ configs/issues/118-avoid-struts.hs
@@ -5,7 +5,6 @@ module Main (main) where
 import XMonad
 import XMonad.Actions.DynamicWorkspaces
 import XMonad.Config.Desktop
-import XMonad.Hooks.ManageDocks
 import XMonad.Prompt
 import XMonad.Util.EZConfig
 
@@ -15,16 +14,7 @@ main = do
     putStrLn "XMonad starting..."
     xmonad cfg
   where
-    cfg = desktopConfig
-
-            { layoutHook = avoidStruts (Tall 1 (3/100) (1/2))
-
-              -- FIXME: should this be in X.C.Desktop?
-            , handleEventHook = docksEventHook
-            }
-
-            `additionalKeysP`
-
+    cfg = desktopConfig `additionalKeysP`
             [ ("M-S-w", addWorkspacePrompt xpcfg)
             ]
 
